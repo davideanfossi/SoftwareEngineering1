d138911fc8c2a3eb79536b0d06ea8271be82a855
GIUSEPPE PELLEGRINO s303999 <s303999@studenti.polito.it>
2022-05-07 16:34:16 +0200

    test server

--- code/server/server.js
+++ code/server/server.js
@@ -1,6 +1,9 @@
 'use strict';
 const express = require('express');
 // init express
+const UserDAO = require('./modules/UserDAO');
+const db = new UserDAO('EzWh');
+
 const app = new express();
 const port = 3001;
 
@@ -13,6 +16,43 @@ app.get('/api/hello', (req,res)=>{
   }
   return res.status(200).json(message);
 });
+
+
+app.post('/api/testdb', async (req,res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(422).json({error: `Empty body request`});
+  }
+  let user = req.body.user;
+  if (user === undefined || user.name === undefined || user.surname === undefined || user.username === undefined || user.type === undefined ||
+              user.name == '' || user.surname == '' || user.username === undefined || user.type === undefined ) {
+
+    return res.status(422).json({error: `Invalid user data`});
+  }
+  
+  await db.newTableUser();
+  db.storeUser(user);
+  return res.status(201).end();
+});
+
+app.get('/api/testdb', async (req,res) => {
+  try {
+    const userlist = await db.getStoredUsers();
+    res.status(200).json(userlist);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/testdb', (req,res) => {
+  try {
+    db.dropTable();
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+
 
 // activate the server
 app.listen(port, () => {
